42ad09945c6963ce683422309dd7d7cf9a43be89
shiliangliang <mysll@sina.com>
2021-04-09 10:55:46 +0800

    update

--- apps/tools/turbogen/module.go
+++ apps/tools/turbogen/module.go
@@ -1,6 +1,7 @@
 package turbogen
 
 import (
+	_ "embed"
 	"fmt"
 	"os"
 	"os/exec"
@@ -12,50 +13,8 @@ import (
 	"github.com/urfave/cli"
 )
 
-var module_tpl = `package {{tolower .Pkg}}
-import (
-	"context"
-	"turboengine/common/utils"
-	"turboengine/core/api"
-	"turboengine/core/module"
-)
- 
-// Module: 		{{.Name}}
-// Auth: 	 	{{.Auth}}
-// Data:	  	{{.Time.Format "2006-01-02 15:04:05"}}
-// Desc:
-type {{.Name}} struct{
-	module.Module
-}
-
-func (m *{{.Name}}) Name() string{
-	return "{{.Name}}"
-}
-
-func (m *{{.Name}}) OnPrepare(s api.Service) error {
-	m.Module.OnPrepare(s)
-	// load module resource
-	// load module resource end
-
-	return nil
-}
-
-func (m *{{.Name}}) OnStart(ctx context.Context) error {
-	m.Module.OnStart(ctx)
-	// subscribe subject
-	// subscribe subject end
-	return nil
-}
-
-func (m *{{.Name}}) OnUpdate(t *utils.Time) {
-
-}
-
-func (m *{{.Name}}) OnStop() error {
-	return nil
-}
-
-`
+//go:embed module.tpl
+var module_tpl string
 
 type ModuleInfo struct {
 	Pkg  string
---
+++ apps/tools/turbogen/module.tpl
@@ -0,0 +1,42 @@
+package {{tolower .Pkg}}
+import (
+	"context"
+	"turboengine/common/utils"
+	"turboengine/core/api"
+	"turboengine/core/module"
+)
+ 
+// Module: 		{{.Name}}
+// Auth: 	 	{{.Auth}}
+// Data:	  	{{.Time.Format "2006-01-02 15:04:05"}}
+// Desc:
+type {{.Name}} struct{
+	module.Module
+}
+
+func (m *{{.Name}}) Name() string{
+	return "{{.Name}}"
+}
+
+func (m *{{.Name}}) OnPrepare(s api.Service) error {
+	m.Module.OnPrepare(s)
+	// load module resource
+	// load module resource end
+
+	return nil
+}
+
+func (m *{{.Name}}) OnStart(ctx context.Context) error {
+	m.Module.OnStart(ctx)
+	// subscribe subject
+	// subscribe subject end
+	return nil
+}
+
+func (m *{{.Name}}) OnUpdate(t *utils.Time) {
+
+}
+
+func (m *{{.Name}}) OnStop() error {
+	return nil
+}
