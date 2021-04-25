715f6a082a99842fe30729346ad4562db63cf160
shiliangliang <mysll@sina.com>
2021-04-25 16:59:20 +0800

    change to zxy(unity order)

--- gameplay/internal/quatutil.go
+++ gameplay/internal/quatutil.go
@@ -11,7 +11,7 @@ func Clamp(value, min, max float64) float64 {
 }
 
 func Euler(eulers mgl32.Vec3) mgl32.Quat {
-	return mgl32.AnglesToQuat(mgl32.DegToRad(eulers.Y()), mgl32.DegToRad(eulers.Z()), mgl32.DegToRad(eulers.X()), mgl32.ZXY)
+	return mgl32.AnglesToQuat(mgl32.DegToRad(eulers.Z()), mgl32.DegToRad(eulers.X()), mgl32.DegToRad(eulers.Y()), mgl32.ZXY)
 }
 
 func QuatMulVec3(q mgl32.Quat, v mgl32.Vec3) mgl32.Vec3 {
@@ -45,8 +45,35 @@ func toEulerYZX(rotation mgl32.Quat) mgl32.Vec3 {
 	return mgl32.Vec3{mgl32.RadToDeg(x), mgl32.RadToDeg(y), mgl32.RadToDeg(z)}
 }
 
+func toEulerZXY(rotation mgl32.Quat) mgl32.Vec3 {
+	var x, y, z float32
+
+	r := rotation.Normalize()
+	te := r.Mat4()
+	m11 := float64(te[0])
+	m12 := float64(te[4])
+	//m13 := float64(te[8])
+	m21 := float64(te[1])
+	m22 := float64(te[5])
+	//m23 := float64(te[9])
+	m31 := float64(te[2])
+	m32 := float64(te[6])
+	m33 := float64(te[10])
+
+	// yzx
+	x = float32(math.Asin(Clamp(m32, -1, 1)))
+	if math.Abs(m32) < 0.9999999 {
+		y = float32(math.Atan2(-m31, m33))
+		z = float32(math.Atan2(-m12, m22))
+	} else {
+		y = 0
+		z = float32(math.Atan2(m21, m11))
+	}
+	return mgl32.Vec3{mgl32.RadToDeg(x), mgl32.RadToDeg(y), mgl32.RadToDeg(z)}
+}
+
 func ToEuler(rotation mgl32.Quat) mgl32.Vec3 {
-	return toEulerYZX(rotation)
+	return toEulerZXY(rotation)
 }
 
 func LookAt(position, target mgl32.Vec3) mgl32.Quat {
