b560e838f8b890b20ecd5043b40ea271a0da1d1c
shiliangliang <mysll@sina.com>
2021-04-27 13:54:31 +0800

    update type

--- common/datatype/type.go
+++ common/datatype/type.go
@@ -36,8 +36,8 @@ func (v Vec3) Equal(rhs Vec3) bool {
 	return true
 }
 
-func (v1 Vec3) Cross(v2 Vec3) Vec3 {
-	return Vec3{v1[1]*v2[2] - v1[2]*v2[1], v1[2]*v2[0] - v1[0]*v2[2], v1[0]*v2[1] - v1[1]*v2[0]}
+func (v Vec3) Cross(v2 Vec3) Vec3 {
+	return Vec3{v[1]*v2[2] - v[2]*v2[1], v[2]*v2[0] - v[0]*v2[2], v[0]*v2[1] - v[1]*v2[0]}
 }
 
 func (v Vec3) Mul(c float32) Vec3 {
@@ -92,31 +92,31 @@ func (v Vec2) Y() float32 {
 	return v[1]
 }
 
-func (v1 Vec2) Add(v2 Vec2) Vec2 {
-	return Vec2{v1[0] + v2[0], v1[1] + v2[1]}
+func (v Vec2) Add(v2 Vec2) Vec2 {
+	return Vec2{v[0] + v2[0], v[1] + v2[1]}
 }
 
-func (v1 Vec2) Sub(v2 Vec2) Vec2 {
-	return Vec2{v1[0] - v2[0], v1[1] - v2[1]}
+func (v Vec2) Sub(v2 Vec2) Vec2 {
+	return Vec2{v[0] - v2[0], v[1] - v2[1]}
 }
 
-func (v1 Vec2) Mul(c float32) Vec2 {
-	return Vec2{v1[0] * c, v1[1] * c}
+func (v Vec2) Mul(c float32) Vec2 {
+	return Vec2{v[0] * c, v[1] * c}
 }
 
-func (v1 Vec2) Dot(v2 Vec2) float32 {
-	return v1[0]*v2[0] + v1[1]*v2[1]
+func (v Vec2) Dot(v2 Vec2) float32 {
+	return v[0]*v2[0] + v[1]*v2[1]
 }
 
-func (v1 Vec2) Len() float32 {
-	return float32(math.Hypot(float64(v1[0]), float64(v1[1])))
+func (v Vec2) Len() float32 {
+	return float32(math.Hypot(float64(v[0]), float64(v[1])))
 }
 
-func (v1 Vec2) LenSqr() float32 {
-	return v1[0]*v1[0] + v1[1]*v1[1]
+func (v Vec2) LenSqr() float32 {
+	return v[0]*v[0] + v[1]*v[1]
 }
 
-func (v1 Vec2) Normalize() Vec2 {
-	l := 1.0 / v1.Len()
-	return Vec2{v1[0] * l, v1[1] * l}
+func (v Vec2) Normalize() Vec2 {
+	l := 1.0 / v.Len()
+	return Vec2{v[0] * l, v[1] * l}
 }
