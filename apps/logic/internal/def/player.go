5c06ef460cb3b3571d1efef68862975ef8df1a5f
shiliangliang <mysll@sina.com>
2021-04-27 11:13:36 +0800

    update

--- apps/logic/internal/def/player.go
+++ apps/logic/internal/def/player.go
@@ -1,14 +1,15 @@
 package def
 
-import "turboengine/gameplay/object"
+import "turboengine/common/datatype"
 
 type Player struct {
-	Name string `attr:"save,public"`
-	Sex  int32  `attr:"save,private"`
-	Hp   int64  `attr:"public,realtime"`
-	Dmg  float32
-	Test float64
-	Pos  object.Vec3 `attr:"public,private,realtime"`
+	Name  string `attr:"save,public"`
+	Sex   int32  `attr:"save,private"`
+	Hp    int64  `attr:"public,realtime"`
+	Dmg   float32
+	Test  float64
+	Pos   datatype.Vec3 `attr:"public,private,realtime"`
+	Bytes []byte
 }
 
 func init() {
