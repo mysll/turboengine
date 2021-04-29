5512f7a72cf4d274156938719fa98a68d041e286
shiliangliang <mysll@sina.com>
2021-04-29 11:35:21 +0800

    update mysql orm

--- core/plugin/storage/driver/mysql.go
+++ core/plugin/storage/driver/mysql.go
@@ -1,1 +1,46 @@
 package driver
+
+import (
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+)
+
+type MysqlDao struct {
+	db *gorm.DB
+}
+
+func (m *MysqlDao) Connect(dsn string) error {
+	db, err := gorm.Open(mysql.New(
+		mysql.Config{
+			DSN:               dsn,
+			DefaultStringSize: 256,
+		}), &gorm.Config{})
+	if err != nil {
+		return err
+	}
+	m.db = db
+	return nil
+}
+
+func (m *MysqlDao) Create(name string, model interface{}) error {
+	return m.db.AutoMigrate(model)
+}
+
+func (m *MysqlDao) Select(id uint64, data interface{}) error {
+	return nil
+}
+func (m *MysqlDao) Insert(id uint64, data interface{}) (uint64, error) {
+	return 0, nil
+
+}
+func (m *MysqlDao) Update(id uint64, data interface{}) error {
+	return nil
+
+}
+func (m *MysqlDao) Del(name string, id uint64) error {
+	return nil
+}
+
+func init() {
+
+}
--- core/plugin/storage/storage.go
+++ core/plugin/storage/storage.go
@@ -1,22 +1,40 @@
 package storage
 
 import (
+	"turboengine/common/log"
 	"turboengine/common/utils"
 	"turboengine/core/api"
 	"turboengine/core/plugin"
+	"turboengine/core/plugin/storage/driver"
+	"turboengine/gameplay/dao"
 )
 
 const (
 	Name = "Storage"
 )
 
+const (
+	DB_OP_CREATE = 1 + iota
+	DB_OP_SELECT
+	DB_OP_INSERT
+	DB_OP_UPDATE
+	DB_OP_DEL
+)
+
 type Driver interface {
+	Connect(dsn string) error
+	Create(name string, model interface{}) error
+	Select(id uint64, data interface{}) error
+	Insert(id uint64, data interface{}) (uint64, error)
+	Update(id uint64, data interface{}) error
+	Del(name string, id uint64) error
 }
 
 type ResultCallback func(interface{}, error)
 
 type Request struct {
 	op   int
+	id   uint64
 	data interface{}
 	cb   ResultCallback
 }
@@ -40,6 +58,23 @@ func (s *Storage) Prepare(srv api.Service, args ...interface{}) {
 	s.id = s.srv.Attach(s.roundInvoke)
 	s.pending = make(chan *Request, 1024)
 	s.done = make(chan *Response, 1024)
+	if dt, ok := args[0].(string); ok {
+		if dsn, ok := args[1].(string); ok {
+			switch dt {
+			case "mysql":
+				s.driver = new(driver.MysqlDao)
+				if err := s.driver.Connect(dsn); err != nil {
+					panic(err)
+				}
+			}
+		}
+	}
+	for n, m := range dao.GetAllModel() {
+		if err := s.driver.Create(n, m); err != nil {
+			panic(err)
+		}
+		log.Info("create model ", n, " ok")
+	}
 }
 
 func (s *Storage) Shut(srv api.Service) {
