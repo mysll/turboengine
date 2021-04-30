9e561e1c20ee14d5a92b731d41f5d5271bb8875f
shiliangliang <mysll@sina.com>
2021-04-30 13:33:29 +0800

    update test

--- common/terrain/grid/grid.go
+++ common/terrain/grid/grid.go
@@ -27,8 +27,8 @@ func (g *Grid) setType(t int) {
 	g.gtype = byte(t)
 }
 
-func (g *Grid) getType() int {
-	return int(g.gtype)
+func (g *Grid) Type() uint32 {
+	return uint32(g.gtype)
 }
 
 func (g *Grid) setHeight(f float32) {
@@ -40,7 +40,7 @@ func (g *Grid) setHeight(f float32) {
 	g.height[2] = b[3]
 }
 
-func (g *Grid) getHeight() float32 {
+func (g *Grid) Height() float32 {
 	b := make([]byte, 4)
 	b[0] = 0
 	b[1] = g.height[0]
@@ -51,18 +51,44 @@ func (g *Grid) getHeight() float32 {
 	return float
 }
 
-type GridMap struct {
-	pixel  Vec2      // 原点坐标(像素)
-	zone   Vec2      // 格子长和宽(像素)
-	unit   Vec2      // n坐标单位=1像素单位
-	max    GridPos   // 格子最大(行,列)
-	origin Vec2      // 原点坐标
-	size   Vec2      // 格子大小
-	grids  [][]*Grid // 格子数据
+type grid uint32
+
+func (g *grid) SetHeight(h uint32) {
+	*g = grid(uint32(*g)&0xFF000000 | (h & 0xFFFFFF))
 }
 
-func NewGridMap(pixel, zone, unit Vec2, grids [][]*Grid) *GridMap {
-	return &GridMap{pixel: pixel, zone: zone, unit: unit, grids: grids}
+func (g *grid) SetType(t uint32) {
+	*g = grid((t << 24) | (uint32(*g) & 0xFFFFFF))
+}
+
+func (g grid) Height() uint32 {
+	return uint32(g) & 0xFFFFFF
+}
+
+func (g grid) Type() uint32 {
+	return uint32(g) >> 24
+}
+
+// 映射到[0,0xFFFFFF]
+func clampHeight(min, max, height, limit float32) uint32 {
+	return uint32((height - min) / (max - min) * limit)
+}
+
+// 映射回[min, max]
+func getClampHeight(min, max, limit float32, height uint32) float32 {
+	return float32(height)*(max-min)/0xFFFF + min
+}
+
+type GridMap struct {
+	pixel     Vec2     // 原点坐标(像素)
+	zone      Vec2     // 格子长和宽(像素)
+	unit      Vec2     // n坐标单位=1像素单位
+	max       GridPos  // 格子最大(行,列)
+	origin    Vec2     // 原点坐标
+	size      Vec2     // 格子大小
+	grids     [][]grid // 格子数据
+	maxHeight float32
+	minHeight float32
 }
 
 func (g *GridMap) init() {
@@ -71,7 +97,8 @@ func (g *GridMap) init() {
 }
 
 func (g *GridMap) LoadFromFile(f string) {
-
+	// var g grid
+	// grid.setHeight(clampHeight(g.minHeight, g.maxHeight,height, 0xFFFFFF)
 }
 
 func (g *GridMap) CanWalk(pos Vec3) (b bool) {
@@ -79,7 +106,7 @@ func (g *GridMap) CanWalk(pos Vec3) (b bool) {
 	if grid == nil {
 		return false
 	}
-	return grid.gtype|MAP_TYPE_WATER|MAP_TYPE_TREE|MAP_TYPE_BUILD != 0
+	return grid.Type()|MAP_TYPE_WATER|MAP_TYPE_TREE|MAP_TYPE_BUILD != 0
 }
 
 func (g *GridMap) LineCanWalk(step float32, start, end Vec3) bool {
@@ -109,7 +136,7 @@ func (g *GridMap) Height(pos Vec3) float32 {
 	if grid == nil {
 		return float32(math.NaN())
 	}
-	return grid.getHeight()
+	return getClampHeight(g.minHeight, g.maxHeight, 0xFFFFFF, grid.Height())
 }
 
 func (g *GridMap) MapType(pos Vec3) int {
@@ -117,10 +144,10 @@ func (g *GridMap) MapType(pos Vec3) int {
 	if grid == nil {
 		return MAP_TYPE_NONE
 	}
-	return grid.getType()
+	return int(grid.Type())
 }
 
-func (g *GridMap) getGrid(pos Vec3) *Grid {
+func (g *GridMap) getGrid(pos Vec3) *grid {
 	x := (pos.X() - g.origin.X()) / g.size.X()
 	y := (pos.Z() - g.origin.Y()) / g.size.Y()
 	row := uint16(math.Ceil(float64(x)))
@@ -128,5 +155,5 @@ func (g *GridMap) getGrid(pos Vec3) *Grid {
 	if row > g.max.Row || col > g.max.Col {
 		return nil
 	}
-	return g.grids[row][col]
+	return &g.grids[row][col]
 }
