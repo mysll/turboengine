be578de26a81f45d647a9f86e99b56c2604b0487
shiliangliang <mysll@sina.com>
2021-09-29 20:07:25 +0800

    fix platform error

--- core/service/capture_panic_linux.go
+++ core/service/capture_panic_linux.go
@@ -1,19 +1,20 @@
 // Log the panic under unix to the log file
 
-//+build unix
+//go:build linux
+// +build linux
 
 package service
 
 import (
-    "log"
-    "os"
-    "syscall"
+	"log"
+	"os"
+	"syscall"
 )
 
 // redirectStderr to the file passed in
 func redirectStderr(f *os.File) {
-    err := syscall.Dup2(int(f.Fd()), int(os.Stderr.Fd()))
-    if err != nil {
-        log.Fatalf("Failed to redirect stderr to file: %v", err)
-    }
+	err := syscall.Dup2(int(f.Fd()), int(os.Stderr.Fd()))
+	if err != nil {
+		log.Fatalf("Failed to redirect stderr to file: %v", err)
+	}
 }
