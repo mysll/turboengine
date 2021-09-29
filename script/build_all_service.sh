a9af6ab785bb1b34005b54085c6fca3a8e2d9dee
shiliangliang <mysll@sina.com>
2021-09-29 17:12:55 +0800

    update script

--- script/build_all_service.sh
+++ script/build_all_service.sh
@@ -4,10 +4,18 @@ source ./style_info.cfg
 
 service_source_root=(
   ../apps/gate/
+  ../apps/global/
+  ../apps/logic/
+  ../apps/login/
+  ../apps/management
 )
 
 service_names=(
   gate_service
+  global_service
+  logic_service
+  login_service
+  cli
 )
 
 bin_dir="../bin"
@@ -26,6 +34,6 @@ for ((i = 0; i < ${#service_source_root[*]}; i++)); do
   cd $begin_path
   service_path=${service_source_root[$i]}
   cd $service_path && echo -e "${SKY_BLUE_PREFIX}Current directory: $PWD $COLOR_SUFFIX"
-  make install && echo -e "${SKY_BLUE_PREFIX}build ${service_names[$i]} success,moving binary file to the bin directory${COLOR_SUFFIX}" &&
+  make install target=linux && echo -e "${SKY_BLUE_PREFIX}build ${service_names[$i]} success,moving binary file to the bin directory${COLOR_SUFFIX}" &&
     echo -e "${SKY_BLUE_PREFIX}Successful moved ${service_names[$i]} to the bin directory${COLOR_SUFFIX}\n"
 done
