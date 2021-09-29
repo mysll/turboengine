c2600c8ca876d695c83eade312f47b5d961fed7f
shiliangliang <mysll@sina.com>
2021-09-29 16:33:28 +0800

    add script

---
+++ script/build_all_service.sh
@@ -0,0 +1,31 @@
+#!/usr/bin/env bash
+
+source ./style_info.cfg
+
+service_source_root=(
+  ../apps/gate/
+)
+
+service_names=(
+  gate_service
+)
+
+bin_dir="../bin"
+logs_dir="../logs"
+
+if [ ! -d $bin_dir ]; then
+  mkdir -p $bin_dir
+fi
+if [ ! -d $logs_dir ]; then
+  mkdir -p $logs_dir
+fi
+
+#begin path
+begin_path=$PWD
+for ((i = 0; i < ${#service_source_root[*]}; i++)); do
+  cd $begin_path
+  service_path=${service_source_root[$i]}
+  cd $service_path && echo -e "${SKY_BLUE_PREFIX}Current directory: $PWD $COLOR_SUFFIX"
+  make install && echo -e "${SKY_BLUE_PREFIX}build ${service_names[$i]} success,moving binary file to the bin directory${COLOR_SUFFIX}" &&
+    echo -e "${SKY_BLUE_PREFIX}Successful moved ${service_names[$i]} to the bin directory${COLOR_SUFFIX}\n"
+done
